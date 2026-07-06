a18f7ff8552a4e08ca8bce009bfa133afbebc500
sistema-lime <jessicalf1@hotmail.com>
2026-07-05 23:30:32 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,2 @@
+self.addEventListener('install',e=>e.waitUntil(caches.open('lime-run-v2').then(c=>c.addAll(['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png']))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
